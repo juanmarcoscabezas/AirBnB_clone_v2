9e63b3107179162c4e498e2f1f4dc656eaa770ff
dantsub <dany.mrtinez@gmail.com>
2020-04-01 01:58:53 +0000

    change position module

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,9 +9,9 @@ from models.base_model import BaseModel, Base  # import class model
 from models.state import State  # import class State
 from models.city import City  # import class City
 from models.user import User  # import class User
-from models.amenity import Amenity  # import class Amenity
 from models.place import Place  # import class Place
 from models.review import Review  # import class Review
+from models.amenity import Amenity  # import class Amenity
 import hashlib
 
 
