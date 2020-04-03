dd9c2c7f2b691d62eba6ab627cd2c7a1ad9b9d5a
dantsub <dany.mrtinez@gmail.com>
2020-04-03 00:24:25 +0000

    update test

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""test for db storage"""
+import unittest
+import pep8
+import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+
+class TestDBStorage(unittest.TestCase):
+    '''this will test the DBStorage'''
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+            self.db = MySQLdb.connect(os.getenv("HBNB_MYSQL_HOST"),
+                                      os.getenv("HBNB_MYSQL_USER"),
+                                      os.getenv("HBNB_MYSQL_PWD"),
+                                      os.getenv("HBNB_MYSQL_DB"))
+            self.cursor = self.db.cursor()
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+
+    def tearDown(self):
+        """teardown"""
+        if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+            self.db.close()
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+    def test_pep8_FileStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+    def test_attributes_DBStorage(self):
+        """Test DBStorage attributes methods"""
+        self.assertTrue(hasattr(DBStorage, '_DBStorage__engine'))
+        self.assertTrue(hasattr(DBStorage, '_DBStorage__session'))
+        self.assertTrue(hasattr(DBStorage, 'new'))
+        self.assertTrue(hasattr(DBStorage, 'save'))
+        self.assertTrue(hasattr(DBStorage, 'all'))
+        self.assertTrue(hasattr(DBStorage, 'delete'))
+        self.assertTrue(hasattr(DBStorage, 'reload'))
+
+
+if __name__ == "__main__":
+    unittest.main()
