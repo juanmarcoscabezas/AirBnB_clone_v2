454c464e1dc6395f3486d4c656f21358d2a65edf
dantsub <dany.mrtinez@gmail.com>
2020-04-03 00:53:09 +0000

    fix imports

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -12,7 +12,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.engine.file_storage import DBStorage
+from models.engine.db_storage import DBStorage
 
 
 class TestDBStorage(unittest.TestCase):
